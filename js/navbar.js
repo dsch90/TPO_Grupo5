dae71e4f49d72288b5fa70d19d95b10ab52819bf
Maria Centurion <centurionmalena94@gmail.com>
2023-05-15 20:01:30 -0300

    js

--- js/navbar.js
+++ js/navbar.js
@@ -7,12 +7,12 @@ class navBar extends HTMLElement{
             <div class="navbar__wrapper">
               <nav class="navbar__menu">
                     <a href="index.html" class="brand">
-                      <img src="" alt="logo" width="40" height="35" class="d-inline-block align-text-top"> CaRent 
+                      <img src="images/coche.png" alt="logo" width="40" height="35" class="d-inline-block align-text-top"> CaRent 
                     </a>
                 <div id="nav-icon3"><span></span><span></span><span></span><span></span></div>
                 <ul class="navbar__nav">
                   <li class="navbar__link after-transform"><a class="menu_link" href="#">Vehiculos</a></li>
-                  <li class="navbar__link after-transform"><a class="menu_link" href="servicios.html">Contacto</a></li>
+                  <li class="navbar__link after-transform"><a class="menu_link" href="/formulario_Carent.html">Contacto</a></li>
                   <li class="navbar__link after-transform"><a class="menu_link" href="#">Acerca de Nosotros</a></li>
                 </ul>
               </nav>
@@ -21,7 +21,7 @@ class navBar extends HTMLElement{
           <div class="navbar-responsive">
             <ul class="navbar-responsive__nav">
               <li class="navbar-responsive__link after-transform"><a href="#">Vehiculos</a></li>
-              <li class="navbar-responsive__link after-transform"><a href="#">Contacto</a></li>
+              <li class="navbar-responsive__link after-transform"><a href="/formulario_Carent.html">Contacto</a></li>
               <li class="navbar-responsive__link after-transform"><a href="#">Acerca de nosotros</a></li>
             </ul>
           </div>
