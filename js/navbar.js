c8763f17e24037feb285d5cb26ee838d06f7c110
Maria Centurion <centurionmalena94@gmail.com>
2023-05-17 18:02:30 -0300

    eliminar cosas innecesarias

--- js/navbar.js
+++ js/navbar.js
@@ -2,15 +2,15 @@
 class navBar extends HTMLElement{
     connectedCallback() {
       this.innerHTML =`
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-          <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+          <div class="container-xl">
               <a class="navbar-brand" href="index.html">
-              <img src="images/coche.png" alt="logo" width="40" height="35" class="d-inline-block align-text-top"> CaRent</a>
+              <img src="/images/logo.png" alt="logo" width="40" height="35" class="d-inline-block align-text-top"> CaRent</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                   <a class="nav-link active" aria-current="page" href="#">Vehiculos</a>
                 </li>
